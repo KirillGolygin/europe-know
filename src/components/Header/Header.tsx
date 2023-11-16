e82aa13699d20648518fe7e4fb44b96c8fcac15e
Kirill Golygin <k.golygin.dev@gmail.com>
2023-11-16 13:02:30 +0300

    realise feature showing different content of popup

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,22 @@
 import { Link } from "react-router-dom";
 
+import { useAppDispatch, useAppSelector } from "../../redux/hooks/redux-hooks";
+
+import { selectPopup } from "../../redux/popups-slice";
+import { togglePopUp, changeType } from "../../redux/popups-slice";
+
 import Popup from "../Popup/Popup";
 
 import "./Header.scss";
 
 const Header = () => {
+  const dispatch = useAppDispatch();
+  const popup = useAppSelector(selectPopup);
+
+  const openPopup = (type: "login" | "signin") => {
+    dispatch(togglePopUp());
+    dispatch(changeType(type));
+  };
   return (
     <header className="header">
       <nav className="navbar">
@@ -14,14 +26,27 @@ const Header = () => {
           </Link>
         </h1>
         <div className="btn-group">
-          <button className="button">Login</button>
-          <button className="button">Register</button>
+          <button className="button" onClick={() => openPopup("login")}>
+            Login
+          </button>
+          <button className="button" onClick={() => openPopup("signin")}>
+            Register
+          </button>
         </div>
       </nav>
-
-      <Popup>
-        <div style={{ width: 400, height: 400, backgroundColor: "#fff" }}></div>
-      </Popup>
+      {popup.isOpen && (
+        <Popup>
+          {popup.type === "login" ? (
+            <div
+              style={{ width: 400, height: 400, backgroundColor: "#fff" }}
+            ></div>
+          ) : (
+            <div
+              style={{ width: 400, height: 400, backgroundColor: "red" }}
+            ></div>
+          )}
+        </Popup>
+      )}
     </header>
   );
 };
