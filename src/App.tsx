7d20a27b38845a2d349be1523ba27c40929b45b2
Kirill Golygin <k.golygin.dev@gmail.com>
2023-11-17 19:15:28 +0300

    create hoc ProtectedRoute

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import { Routes, Route } from "react-router";
 
+import ProtectedRoute from "./hoc/ProtectedRoute";
+
+import { useAppSelector } from "./redux/hooks/redux-hooks";
+import { selectCurrentUser } from "./redux/users-slice";
+
 import Main from "./pages/Main/Main";
 import CountryDetail from "./pages/CountryDetail/CountryDetail";
 import MyCountries from "./pages/MyCountries/MyCountries";
@@ -9,13 +14,22 @@ import Layout from "./components/Layout/Layout";
 import "./App.scss";
 
 function App() {
+  const user = useAppSelector(selectCurrentUser);
   return (
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Main />} />
           <Route path="/details" element={<CountryDetail />} />
-          <Route path="/my-countries" element={<MyCountries />} />
+
+          <Route
+            path="/my-countries"
+            element={
+              <ProtectedRoute role={user} to="/">
+                <MyCountries />
+              </ProtectedRoute>
+            }
+          />
         </Route>
       </Routes>
     </>
