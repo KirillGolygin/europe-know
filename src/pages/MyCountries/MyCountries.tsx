0c2380ec8d265f4900b7b184e629e17333e20ef0
Kirill Golygin <k.golygin.dev@gmail.com>
2023-11-18 12:41:31 +0300

    add favourites empty text

--- src/pages/MyCountries/MyCountries.tsx
+++ src/pages/MyCountries/MyCountries.tsx
@@ -1,12 +1,21 @@
+import { useAppSelector } from "../../redux/hooks/redux-hooks";
+
+import { selectFavourites } from "../../redux/countries-slice";
+
 import FavouritesList from "../../components/FavouritesList/FavouritesList";
 
 import "./MyCountries.scss";
 
 const MyCountries = () => {
+  const favourites = useAppSelector(selectFavourites);
   return (
-    <>
-      <FavouritesList />
-    </>
+    <div>
+      {favourites.length !== 0 ? (
+        <FavouritesList />
+      ) : (
+        <p className="empty-fav">Список избранных пусть...</p>
+      )}
+    </div>
   );
 };
 
