de2ce5693a2b1cfb6c9b3e346b30a9d6fb4d14a3
Kirill Golygin <k.golygin.dev@gmail.com>
2023-11-19 13:16:02 +0300

    add layout to countryDetail page

--- src/pages/CountryDetail/CountryDetail.tsx
+++ src/pages/CountryDetail/CountryDetail.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useParams } from "react-router";
 
 import { getCountryInfo } from "../../api";
@@ -59,21 +59,21 @@ const CountryDetail = () => {
   if (!pickedCountry) return;
   return (
     <div className="details-container">
-      <div className="text">
+      <div className="info-container">
         <div className="name">
-          <h3 className="text"></h3>
+          <h3 className="text">{pickedCountry.name.common}</h3>
           <Star
             countryName={pickedCountry.name.common}
             favourite={pickedCountry.favourite}
           />
         </div>
 
-        <div className="info-container">
+        <div className="info">
           <p className="title">capital:</p>
           <p className="text">{pickedCountry.capital}</p>
         </div>
 
-        <div className="info-container">
+        <div className="info">
           <p className="title">Currencies:</p>
           <p className="text">
             {pickedCountry.currencies.map((cur) => (
@@ -82,12 +82,12 @@ const CountryDetail = () => {
           </p>
         </div>
 
-        <div className="info-container">
+        <div className="info">
           <p className="title">Region:</p>
           <p className="text">{pickedCountry.region}</p>
         </div>
 
-        <div className="info-container">
+        <div className="info">
           <p className="title">Languages:</p>
           <p className="text">
             {pickedCountry.languages.map((lang) => (
@@ -96,12 +96,34 @@ const CountryDetail = () => {
           </p>
         </div>
 
-        <div className="info-container">
+        <div className="info">
           <p className="title">Population:</p>
           <p className="text">{pickedCountry.population}</p>
         </div>
       </div>
-      <div className="pics"></div>
+      <div className="pics-container">
+        <div className="pic">
+          <p className="text">Flag</p>
+          <div className="img-container">
+            <img
+              className="img"
+              src={pickedCountry.flags.png}
+              alt={pickedCountry.flags.alt}
+            />
+          </div>
+        </div>
+
+        <div className="pic">
+          <p className="text">Coat of arms</p>
+          <div className="img-container">
+            <img
+              className="img"
+              src={pickedCountry.coatOfArms.png}
+              alt={`coat of arms of ${pickedCountry.name.common}`}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
